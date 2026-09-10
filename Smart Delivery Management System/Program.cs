9fa05ffd04364e0f4d2615a21f70a9ceceee97dd
Mohamed-Mahmoud1 <mohamedzanaty.dev@gmail.com>
2026-09-10 22:13:27 +0300

    Theoretical Questions-Question01

--- Smart Delivery Management System/Program.cs
+++ Smart Delivery Management System/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 : Theoretical Questions
+
+            #region Question01
+            //public struct DeliveryAddress
+            //{
+            //    public string City;
+            //    public string Street;
+            //}
+
+            //public class Customer
+            //{
+            //    public string Name;
+            //}
+
+            //a)What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
+            //Answer: When a DeliveryAddress variable is copied into another variable, a new instance of the struct is created. If the copy is modified, it does not affect the original variable because structs are value types.
+
+            //b)What happens when a Customer variable is copied into another variable and one variable modifies the object
+            //Answer: When a Customer variable is copied into another variable, both variables refer to the same object in memory because classes are reference types. If one variable modifies the object, the changes will be reflected in the other variable as well.
+
+
+            #endregion
+
+            #endregion
         }
     }
 }
